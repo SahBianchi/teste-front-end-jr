6a56606e40e28ccb3a14d76f7baf55a70622410d
SahBianchi <samantha.sbianchi@gmail.com>
2024-02-06 19:35:19 -0300

    :lipstick: style: Add componentes e alterando styles

--- src/components/CardsProducts/index.js
+++ src/components/CardsProducts/index.js
@@ -5,25 +5,35 @@ import Button from "../Button";
 export default function CardsProducts(){
     return(
         <div className={styles.containerCardsProducts}>
-            <div className={styles.containerCardProducts}>
-                <div className={styles.containerTxtProducts}>
-                    <h3>Produtos</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur</p>
-                    <Button type="btCheckOut" onClick={()=>{
-                        console.log("fui clicado")
-                    }}>Confira</Button>
-                </div>
-            </div>
 
-            <div className={styles.containerCardProducts}>
-                <div className={styles.containerTxtProducts}>
-                    <h3>Produtos</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur</p>
-                    <Button type="btCheckOut" onClick={()=>{
-                        console.log("fui clicado")
-                    }}>Confira</Button>
+            <div className={styles.contentTest}>
+                <hr />
+                    <h2>Produtos relacionados</h2>
+                <hr />
+            </div>
+            <p><a href="">Ver todos</a></p>
+
+            <div className={styles.containerCards}>
+                <div className={styles.containerCardProducts}>
+                    <div className={styles.containerTxtProducts}>
+                        <h3>Produtos</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                        <Button type="btCheckOut" onClick={()=>{
+                            console.log("fui clicado")
+                        }}>Confira</Button>
+                    </div>
                 </div>
-            </div>  
+
+                <div className={styles.containerCardProducts}>
+                    <div className={styles.containerTxtProducts}>
+                        <h3>Produtos</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                        <Button type="btCheckOut" onClick={()=>{
+                            console.log("fui clicado")
+                        }}>Confira</Button>
+                    </div>
+                </div>  
+            </div>
         </div>
     )
 }
--- src/components/NavBrands/index.js
+++ src/components/NavBrands/index.js
@@ -1,46 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./style.module.css";
 
 import BrandLogo from "../../pictures/LogoNavBrands.png"
+import ArrayNext from "../../pictures/ArrayNext.png"
+import ArrayPrev from "../../pictures/ArrayPrev.png"
 
 export default function NavBrands(){
+    const [containerWidth, setContainerWidth] = useState(0);
+  const [scrollAmount, setScrollAmount] = useState(0);
+  const [numLogos, setNumLogos] = useState(0);
+
+  useEffect(() => {
+    const container = document.querySelector(`.${styles.containerCarousel}`);
+    if (container) {
+      setContainerWidth(container.offsetWidth);
+      setNumLogos(container.children.length);
+    }
+  }, []);
+
+  const handleNextClick = () => {
+    if (scrollAmount < containerWidth * (numLogos - 1)) {
+      setScrollAmount(scrollAmount + containerWidth);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (scrollAmount > 0) {
+      setScrollAmount(scrollAmount - containerWidth);
+    }
+  };
+
     return(
         <div className={styles.containerBrands}>
             <h2>Navegue por marcas</h2>
 
-            <div className={styles.containerCircleBrand}>
-                <div className={styles.circleBrand}>
-                    <img src={BrandLogo}/>
+            <div className={styles.containerCarousel}>
+                <div className={styles.carousel} style={{ transform: `translateX(-${scrollAmount}px)` }}>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 1"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 2"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
+                    <div className={styles.logo}><img src={BrandLogo} alt="Logo 3"/></div>
                 </div>
+                <button className={`${styles.buttonCarrosel} ${styles.prev}`} onClick={handlePrevClick}><img src={ArrayPrev} alt="Seta para voltar"/></button>
 
-                <div className={styles.circleBrand}>
-                    <img src={BrandLogo}/>
-                </div> 
-
-                <div className={styles.circleBrand}>
-                    <img src={BrandLogo} className={styles.diferente}/>
-                </div> 
-
-                <div className={styles.circleBrand}>
-                    <img src={BrandLogo}/>
-                </div> 
-
-                <div className={styles.circleBrand}>
-                    <img src={BrandLogo}/>
-                </div> 
-
-                {/* <div className={styles.circleBrand}>
-                    <img src={BrandLogo}/>
-                </div>  */}
-
-                {/* <div className={styles.circleBrand}>
-                    <img src={BrandLogo}/>
-                </div> 
-
-                <div className={styles.circleBrand}>
-                    <img src={BrandLogo}/>
-                </div>  */}
-            </div>       
+                <button className={`${styles.buttonCarrosel} ${styles.next}`} onClick={handleNextClick}><img src={ArrayNext} alt="Seta para ir para frente"/></button>
+            </div>
         </div>
     )
 }
+
+
+
+
+
