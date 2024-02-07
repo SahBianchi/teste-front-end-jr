7573bf2e08490683acadb98c1616d1a83bad8e71
SahBianchi <samantha.sbianchi@gmail.com>
2024-02-07 20:03:26 -0300

    Atualizando o componente Products para acessar o json

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-* EM PRODUÇÃO
+# Teste Econverse: Vaga Desenvolvedor Front-End Jr
+
+Este é um projeto desenvolvido para o teste da Vaga Desenvolvedor Front-End Jr da Econverse.
+
+#### Especificações Técnicas
+- Desenvolver a pagina em React e TypeScript conforme o [layout](https://www.figma.com/file/rWnzPeoxgynuNPsJjV0VmV/Teste-Front-End-Jr?node-id=0%3A1). 
+- Montar a [vitrine](https://app.econverse.com.br/teste-front-end/junior/tecnologia/layout/vitrine-produtos.png) de produtos consumindo as informações dos produtos em json atraves desse [Link](https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json).
+- Desenvolver a interação ao clicar em um produto conforme layout. A interação consiste em abrir um modal com as principais informações do produto presente no arquivo [JSON](https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json) conforme o produto que clicar.
+- Utilizar Pré-processador Sass, Less ou Stylus.
+- Respeitar o Layout pixel a pixel, tamanho das fontes, cores e botões.
+- Não Utilizar bibliotecas UI como Bootstrap, Foundation, ou afins.
+
+#### Pontos Extras
+- Utilizar Boas práticas de SEO
+- Uso de HTML semântico
+
+## Organização do projeto:
+Para manter uma organização eficiente durante o desenvolvimento, recorri à ferramenta Trello. Utilizei-a para mapear minhas tarefas, identificar pendências e detalhar os componentes necessários, garantindo uma visão abrangente do progresso do projeto.
+
+<p align="center">
+    <img src="./src/pictures/organizacaoTrello.jpeg" width="600" height="auto">
+</p><br>
+
+
+## 🚀 Começando
+
+Essas instruções permitirão que você consiga ter esse projeto em operação na sua máquina local para fins de desenvolvimento e teste.
+
+### 📋 Pré-requisitos para instalação 🔧
+
+* Clone o repositório [teste-front-end-jr](https://github.com/SahBianchi/teste-front-end-jr)
+* Entre na pasta do projeto e instale via terminal utilizando o comando: npm install.
+* Após a instalação ainda no terminal digite o comando: npm start 
+<br>
+<br>
+
+## Agradecimento
+Quero expressar minha sincera gratidão pela oportunidade de participar deste teste. Foi um desafio estimulante e enriquecedor. A estética do layout é excepcional, e fiz o possível para respeitar todos os detalhes com cuidado e atenção.
+<br>
+
+Reconheço que há áreas no código que podem ser aprimoradas e refatoradas, mas dediquei meu melhor esforço dentro do prazo estipulado. Espero que apreciem o resultado final tanto quanto eu apreciei o processo de criação.
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -4,130 +4,47 @@ import Carousel from "../Carousel";
 import Button from "../Button";
 import Modal from "../Modal";
 
-import ImgProduct from "../../pictures/produt1.png"
-
-export default function Products(){
+export default function Products() {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [productData, setProductData] = useState([]);
 
     const openModal = () => {
         setIsModalOpen(true);
     };
-    
-    // const [productData, setProductData] = useState([]);
 
-    // useEffect(() => {
-    //     const fetchProductData = async () => {
-    //         try {
-    //             const response = await fetch('https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json');
-    //             if (!response.ok) {
-    //                 throw new Error('Falha ao carregar os dados do produto');
-    //             }
-    //             const data = await response.json();
-    //             setProductData(data);
-    //             console.log(data)
-    //         } catch (error) {
-    //             console.error('Erro ao buscar dados do produto:', error);
-    //         }
-    //     };
-    //     fetchProductData();
-    // }, []);
+    useEffect(() => {
+        const fetchProductData = async () => {
+            try {
+                const response = await fetch('/teste-front-end/junior/tecnologia/lista-produtos/produtos.json');
+                if (!response.ok) {
+                    throw new Error('Falha ao carregar os dados do produto');
+                }
+                const data = await response.json();
+                setProductData(data.products);
+            } catch (error) {
+                console.error('Erro ao buscar dados do produto:', error);
+            }
+        };
+        fetchProductData();
+    }, []);
 
-    return(
+    return (
         <div className={styles.containerBoxProducts}>
             <Carousel type="containerCarouselProducts">
-            <div className={styles.boxProduct}>
-                    <img src={ImgProduct}/>
-                    <div className={styles.containerInfoProd}>
-                        <p className={styles.descriptionInfoProd}>descrição</p>
-                        <p className={styles.previousPrice}>R$ 30,90</p>
-                        <p className={styles.currentP1rice}>R$ 10,00</p>
-                        <p className={styles.infoParcel}>ou 2x de R$ 49,95 sem juros</p>
-                        <p className={styles.infoShipping}>Frete grátis</p>
+                {productData.map((product) => (
+                    <div key={product.productName} className={styles.boxProduct}>
+                        <img src={product.photo} alt={product.descriptionShort} />
+                        <div className={styles.containerInfoProd}>
+                            <p className={styles.descriptionInfoProd}>{product.descriptionShort}</p>
+                            <p className={styles.previousPrice}>R$ {product.price}</p>
+                            <p className={styles.infoParcel}>ou 2x de R$ {(product.price / 2).toFixed(2)} sem juros</p>
+                            <p className={styles.infoShipping}>Frete grátis</p>
+                        </div>
+                        <Button type="btProduct" onClick={openModal}>Comprar</Button>
                     </div>
-
-                    <Button type="btProduct" onClick={openModal}>Comprar</Button>                
-                </div>
-
-                <div className={styles.boxProduct}>
-                    <img src={ImgProduct}/>
-                    <div className={styles.containerInfoProd}>
-                        <p className={styles.descriptionInfoProd}>descrição</p>
-                        <p className={styles.previousPrice}>R$ 30,90</p>
-                        <p className={styles.currentP1rice}>R$ 10,00</p>
-                        <p className={styles.infoParcel}>ou 2x de R$ 49,95 sem juros</p>
-                        <p className={styles.infoShipping}>Frete grátis</p>
-                    </div>
-
-                    <Button type="btProduct" onClick={()=>{
-                        console.log("fui clicado")
-                    }}>Comprar</Button>                
-                </div>
-
-                <div className={styles.boxProduct}>
-                    <img src={ImgProduct}/>
-                    <div className={styles.containerInfoProd}>
-                        <p className={styles.descriptionInfoProd}>descrição</p>
-                        <p className={styles.previousPrice}>R$ 30,90</p>
-                        <p className={styles.currentP1rice}>R$ 10,00</p>
-                        <p className={styles.infoParcel}>ou 2x de R$ 49,95 sem juros</p>
-                        <p className={styles.infoShipping}>Frete grátis</p>
-                    </div>
-
-                    <Button type="btProduct" onClick={()=>{
-                        console.log("fui clicado")
-                    }}>Comprar</Button>                
-                </div>
-
-                <div className={styles.boxProduct}>
-                    <img src={ImgProduct}/>
-                    <div className={styles.containerInfoProd}>
-                        <p className={styles.descriptionInfoProd}>descrição</p>
-                        <p className={styles.previousPrice}>R$ 30,90</p>
-                        <p className={styles.currentP1rice}>R$ 10,00</p>
-                        <p className={styles.infoParcel}>ou 2x de R$ 49,95 sem juros</p>
-                        <p className={styles.infoShipping}>Frete grátis</p>
-                    </div>
-
-                    <Button type="btProduct" onClick={()=>{
-                        console.log("fui clicado")
-                    }}>Comprar</Button>                
-                </div>
-
-                <div className={styles.boxProduct}>
-                    <img src={ImgProduct}/>
-                    <div className={styles.containerInfoProd}>
-                        <p className={styles.descriptionInfoProd}>descrição</p>
-                        <p className={styles.previousPrice}>R$ 30,90</p>
-                        <p className={styles.currentP1rice}>R$ 10,00</p>
-                        <p className={styles.infoParcel}>ou 2x de R$ 49,95 sem juros</p>
-                        <p className={styles.infoShipping}>Frete grátis</p>
-                    </div>
-
-                    <Button type="btProduct" onClick={()=>{
-                        console.log("fui clicado")
-                    }}>Comprar</Button>                
-                </div>
-
+                ))}
             </Carousel>
             {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} />}
-            {/* {productData.map((product, index) => (
-                <div className={styles.boxProduct}>
-                    <img src={ImgProduct}/>
-                    <div className={styles.containerInfoProd}>
-                        <p className={styles.descriptionInfoProd}>descrição</p>
-                        <p className={styles.previousPrice}>R$ 30,90</p>
-                        <p className={styles.currentP1rice}>R$ 10,00</p>
-                        <p className={styles.infoParcel}>ou 2x de R$ 49,95 sem juros</p>
-                        <p className={styles.infoShipping}>Frete grátis</p>
-                    </div>
-
-                    <Button type="btProduct" onClick={()=>{
-                        console.log("fui clicado")
-                    }}>Comprar</Button>                
-                </div>
-            ))} */}
-            
-
         </div>
-    )
+    );
 }
